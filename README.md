2459877cb2eccfedb7eb0f6072f872bab888f42c
Rafael Sant Anna <164666961+Rafael-Sant-Anna@users.noreply.github.com>
2025-07-09 22:50:41 +0000

    adiciona scripts e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Treinando Programação de ponta a ponta.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
-function Home () {
-    return <h1> Natalya, eu amo você. Se você me ama, dá uma risadinha!</h1>
+function Home() {
+  return <h1> Natalya, eu amo você. Se você me ama, dá uma risadinha!</h1>;
 }
-
 export default Home;
